9621da403106a14ad94973fc956d39070d953f46
@Cornelio <jun23.llagas@gmail.com>
2025-01-15 15:48:06 +0800

    Retain customTipValue

--- src/components/BillSplitter-app.vue
+++ src/components/BillSplitter-app.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container">
-        <div class="splitter">
+    <div class="bill splitter">
+        <div class="container">
             <div class="input-section">
                 <div class="input-group">
                     <label for="bill">Bill</label>
@@ -9,28 +9,33 @@
                 <div class="input-group">
                     <label>Select Tip %</label>
                     <div class="tip-options">
-                        <button v-for="tip in tipOptions" :key="tip" @click="selectTip(tip)">{{ tip }}%</button>
-                        <!-- <button @click="customTip = true">Custom</button> -->
-                        <input id="customTipValue" v-model.number="customTipValue" placeholder="Custom" />
+                        <button v-for="tip in tipOptions" :key="tip" @click="selectTip(tip)"
+                         :class="{ active: selectedTip === tip }">{{ tip
+                            }}%</button>
+                        <input 
+                        id="customTipValue" v-model.number="customTipValue" @input="onCustomTipInput"
+                        placeholder="Custom" />
                     </div>
                     <div class="input-group">
                         <label for="people" id="people">Number of People</label>
-                        <input id="people" v-model="people">
+                        <input id="people" v-model.number="people">
                     </div>
                 </div>
             </div>
             <div class="output-section">
                 <div class="output-group">
-                    <label>Tip Amount / person</label>
+                    <p>Tip Amount / person</p>
                     <div class="output-value">{{ formattedTipAmountPerPerson }}
                     </div>
                 </div>
                 <div class="output-group">
-                    <label>Total / person</label>
+                    <p>Total / person</p>
                     <div class="output-value">{{ formattedTotalPerPerson }}
                     </div>
                 </div>
-                <button @click="reset">RESET</button>
+                <button class="reset-button" :disabled="!canReset" @click="reset">
+                    RESET
+                </button>
             </div>
         </div>
     </div>
@@ -44,7 +49,8 @@ export default {
             tipOptions: [5, 10, 15, 25, 50],
             selectedTip: 15,
             customTipValue: null,
-            people: 5
+            people: 5,
+            isCustomTipActive: false,
         };
     },
     computed: {
@@ -54,7 +60,7 @@ export default {
             let peopleCount = parseFloat(this.people) || 1;
 
             let tipPercentage;
-            if (this.customTipValue > 0) {
+            if (this.isCustomTipActive && this.customTipValue > 0) {
                 tipPercentage = this.customTipValue;
             } else {
                 tipPercentage = this.selectedTip;
@@ -73,7 +79,7 @@ export default {
 
             let tipPercentage;
             let totalTipAmount;
-            if (this.customTipValue > 0) {
+            if (this.isCustomTipActive && this.customTipValue > 0) {
                 tipPercentage = this.customTipValue;
             } else {
                 tipPercentage = this.selectedTip;
@@ -106,29 +112,57 @@ export default {
                 return this.totalPerPerson;
             }
         },
+        canReset(){
+            // Ternary operator
+             return this.bill || this.people || this.selectedTip || this.customTipValue;
+
+             //same as above code 
+            // if (this.bill || this.people || this.selectedTip || this.customTipValue){
+            //   return true;
+            // } else {
+            //   return false;
+            // }
+        },
     },
     methods: {
         selectTip(tip) {
             this.selectedTip = tip;
-            this.customTipValue = null; // Clear customTipValue when a standard tip is selected
+            // this.customTipValue = null; // Clear customTipValue when a standard tip is selected
+            
+            this.isCustomTipActive = false;
+        },
+        onCustomTipInput(){
+          this.isCustomTipActive = true;
         },
         reset() {
             this.bill = null;
             this.selectedTip = null;
             this.customTipValue = null;
             this.people = 1;
+            this.isCustomTipActive = false; 
         }
     }
 }
 </script>
 
 <style scoped>
-.splitter {
+.container{
+    display: flex;
     background-color: var(--white);
     border-radius: 15px;
+    box-shadow: 0 10px 15px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
 }
 
-label {
-    color: var(--very-dark-cyan);
+.input-section{
+    background-color: var(--white);
+}
+.output-section {
+    background-color: var(--very-dark-cyan);
+    color: var(--white);
+}
+.input-section, .output-section{
+ padding: 2rem;
+ flex: 1;
 }
 </style>
